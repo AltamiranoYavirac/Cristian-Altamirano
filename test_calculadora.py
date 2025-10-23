a065ee0e2b02249af8ce8071520c04da674c6b1a
Cristian Altamirano <cav.altamirano@yavirac.edu.ec>
2025-10-23 09:52:49 -0500

    Workflow Arreglado

--- test_calculadora.py
+++ test_calculadora.py
@@ -2,7 +2,7 @@
 from calculadora import suma,multiplicacion
 
 def test_suma():
-    assert suma(2,5) == 5
+    assert suma(2,5) == 7
 
 def test_multiplicacion():
-    assert suma(3,5) == 20
+    assert suma(3,5) == 15
